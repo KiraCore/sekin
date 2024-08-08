47ad9b5ddf920516514b7b6de84747ce2b2ee21e
PeepoFrog <organizm999@gmail.com>
2024-08-08 19:20:21 +0300

    feat (updater): add sigkill container and cmd executor to updater

--- src/updater/internal/upgrade_manager/docker/docker.go
+++ src/updater/internal/upgrade_manager/docker/docker.go
@@ -1,10 +1,18 @@
 package docker
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+const (
+	SIGKILL string = "SIGKILL" // 9 - interx
+	SIGTERM string = "SIGTERM" // 15 - sekai
 )
 
 func CheckContainerState(cli *client.Client, containerID string) (string, error) {
@@ -24,4 +32,46 @@ func CheckContainerState(cli *client.Client, containerID string) (string, error)
 
 	// Return the container state status
 	return state.Status, nil
+}
+
+func KillContainerWithSigkill(ctx context.Context, cli *client.Client, containerID, signal string) error {
+
+	err := cli.ContainerKill(ctx, containerID, signal)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExecInContainer(ctx context.Context, cli *client.Client, containerID string, command []string) ([]byte, error) {
+	execConfig := container.ExecOptions{
+		Cmd:          command,
+		AttachStdout: true,
+		AttachStderr: true,
+		Detach:       false,
+	}
+	execCreateResponse, err := cli.ContainerExecCreate(ctx, containerID, execConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create container exec instance for container %s: %w", containerID, err)
+	}
+
+	execAttachConfig := container.ExecStartOptions{}
+	resp, err := cli.ContainerExecAttach(ctx, execCreateResponse.ID, execAttachConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to attach to container exec instance %s: %w", execCreateResponse.ID, err)
+	}
+	defer resp.Close()
+
+	var outBuf, errBuf bytes.Buffer
+	_, err = stdcopy.StdCopy(&outBuf, &errBuf, resp.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to copy output from container exec: %w", err)
+	}
+
+	if len(errBuf.Bytes()) > 0 {
+		return nil, fmt.Errorf(errBuf.String())
+	}
+
+	output := outBuf.Bytes()
+	return output, nil
 }
