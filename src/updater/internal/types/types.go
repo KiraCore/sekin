9e7bdc30f6a567ebf6aa19433904fa1e62c1af19
PeepoFrog <organizm999@gmail.com>
2024-08-08 19:19:40 +0300

    feat (updater): add sekai hard fork upgrade handler

--- src/updater/internal/types/types.go
+++ src/updater/internal/types/types.go
@@ -20,8 +20,41 @@ type (
 	}
 )
 
+type UpgradePlanResource struct {
+	Checksum string `json:"checksum"`
+	ID       string `json:"id"`
+	URL      string `json:"url"`
+	Version  string `json:"version"`
+}
+
 type UpgradePlan struct {
-	Plan interface{} `json:"plan"`
+	InstateUpgrade            bool                  `json:"instateUpgrade"`
+	MaxEnrolmentDuration      string                `json:"maxEnrolmentDuration"`
+	Name                      string                `json:"name"`
+	NewChainID                string                `json:"newChainId"`
+	OldChainID                string                `json:"oldChainId"`
+	ProcessedNoVoteValidators bool                  `json:"processedNoVoteValidators"`
+	ProposalID                string                `json:"proposalID"`
+	RebootRequired            bool                  `json:"rebootRequired"`
+	Resources                 []UpgradePlanResource `json:"resources"`
+	RollbackChecksum          string                `json:"rollbackChecksum"`
+	SkipHandler               bool                  `json:"skipHandler"`
+	UpgradeTime               string                `json:"upgradeTime"`
+}
+
+type PlanData struct {
+	Plan UpgradePlan `json:"plan"`
 }
 
 const SEKIN_LATEST_COMPOSE_URL = "https://raw.githubusercontent.com/KiraCore/sekin/main/compose.yml"
+const SEKAI_CONTAINER_ID = "sekin-sekai-1"
+
+const UPDATE_PLAN string = "./upgradePlan.json"
+
+const SEKIN_HOME string = "/home/km/sekin"
+const SEKAI_HOME string = "/home/km/sekin" + CONTAINERIZED_SEKAI_HOME
+
+const CONTAINERIZED_SEKAI_HOME string = "/sekai"                               //from sekai container prospective
+const CONTAINERIZED_SEKAI_CONFIG string = CONTAINERIZED_SEKAI_HOME + "/config" //from sekai container prospective
+
+const SEKAI_IMAGE_NAME string = "ghcr.io/kiracore/sekin/sekai"
