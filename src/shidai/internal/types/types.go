bbef26d535e68ef9f4fdc1278df142f13e4d713c
PeepoFrog <organizm999@gmail.com>
2024-09-10 19:02:22 +0300

    feat (config_editor): kill cmd handler

--- src/shidai/internal/types/types.go
+++ src/shidai/internal/types/types.go
@@ -292,6 +292,9 @@ const (
 	DirPermWR os.FileMode = 0755
 
 	SEKIN_LATEST_COMPOSE_URL = "https://raw.githubusercontent.com/KiraCore/sekin/main/compose.yml"
+
+	SIGKILL string = "SIGKILL" // 9 - interx
+	SIGTERM string = "SIGTERM" // 15 - sekai
 )
 
 var (
