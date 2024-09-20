d21603d758219a9b98c95ab5b0a9095dacb96049
PeepoFrog <organizm999@gmail.com>
2024-09-20 17:50:51 +0300

    feat (prometheus_exporter): add new 'metrics' endpoint for api handler

--- src/shidai/internal/api/api.go
+++ src/shidai/internal/api/api.go
@@ -6,8 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/kiracore/sekin/src/shidai/internal/commands"
 	"github.com/kiracore/sekin/src/shidai/internal/logger"
+	prometheusexporter "github.com/kiracore/sekin/src/shidai/internal/prometheus_exporter"
 	"github.com/kiracore/sekin/src/shidai/internal/types"
 	"github.com/kiracore/sekin/src/shidai/internal/update"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 )
 
@@ -19,6 +21,7 @@ func Serve() {
 
 	router := gin.New()
 	router.Use(gin.Recovery())
+	prometheusCustomRegistry := prometheusexporter.RegisterMetrics()
 
 	router.POST("/api/execute", commands.ExecuteCommandHandler)
 	router.GET("/logs/shidai", streamLogs(types.ShidaiLogPath))
@@ -28,10 +31,14 @@ func Serve() {
 	router.GET("/dashboard", getDashboardHandler())
 	router.POST("/config", getCurrentConfigs())
 	router.PUT("/config", setConfig())
+	router.GET("/metrics", gin.WrapH(promhttp.HandlerFor(prometheusCustomRegistry, promhttp.HandlerOpts{}))) // Custom metrics only
+
 	updateContext := context.Background()
 
 	go backgroundUpdate()
 	go update.UpdateRunner(updateContext)
+	go prometheusexporter.RunPrometheusExporterService(updateContext)
+
 	if err := router.Run(":8282"); err != nil {
 		log.Error("Failed to start the server", zap.Error(err))
 	}
