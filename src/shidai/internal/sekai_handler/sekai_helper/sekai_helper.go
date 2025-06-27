905d89101c8acc2bf67e2c6b98b7e37187f4b39f
PeepoFrog <organizm999@gmail.com>
2025-06-27 21:27:21 +0300

    feat (interx_new_init_logic): add net_info endpoint retriever

--- src/shidai/internal/sekai_handler/sekai_helper/sekai_helper.go
+++ src/shidai/internal/sekai_handler/sekai_helper/sekai_helper.go
@@ -15,7 +15,10 @@ import (
 	"go.uber.org/zap"
 )
 
-const endpointStatus string = "status"
+const (
+	endpointStatus  string = "status"
+	endpointNetInfo string = "net_info"
+)
 
 var (
 	log = logger.GetLogger()
@@ -71,4 +74,23 @@ func CheckSekaiStart(ctx context.Context) error {
 		}
 		time.Sleep(requestCooldown)
 	}
+}
+
+func GetNetInfo(ctx context.Context, ipAddress, rpcPort string) (*sekai.NetInfo, error) {
+	url := fmt.Sprintf("http://%s:%s/%s", ipAddress, rpcPort, endpointNetInfo)
+	client := &http.Client{}
+	log.Debug("Querying sekai status by url:", zap.String("url", url))
+
+	body, err := httpexecutor.DoHttpQuery(ctx, client, url, "GET")
+	if err != nil {
+		return nil, err
+	}
+
+	var response *sekai.NetInfo
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
